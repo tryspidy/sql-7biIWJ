9b69292dd5dd9ffc3e540bba56cf4a7bb727c873
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-07 11:14:41 +0530

    Initial commit

---
+++ main.sql
@@ -0,0 +1,3 @@
+SQL is used to communicate with a database.
+According to ANSI (American National Standards Institute), it is the standard language for relational database management systems.
+SQL statements are used to perform tasks such as update data on a database, or retrieve data from a database.
